46d5ec374ae0df6bc480cf153db68e65f2056fe7
ZVitaliy <zakharkinvv@gmail.com>
2020-04-06 21:20:24 +0300

    First commit

---
+++ auth.py
@@ -0,0 +1,17 @@
+'''
+HTTP/2 404
+date: Sun, 05 Apr 2020 17:51:02 GMT
+content-type: text/html; charset=UTF-8
+set-cookie: __cfduid=d8c14f2623302f9b84ed1be36ca0cb4111586109062; expires=Tue, 05-May-20 17:51:02 GMT; path=/; domain=.xhamster.com; HttpOnly; SameSite=Lax
+vary: Accept-Encoding
+set-cookie: stats_id=87100; expires=Sun, 12-Apr-2020 17:51:02 GMT; Max-Age=604800; path=/; domain=.xhamster.com
+srv: m85
+set-cookie: prs=--; expires=Mon, 06-Apr-2020 17:51:02 GMT; Max-Age=86400; path=/; domain=.xhamster.com
+cache-control: no-cache, no-store, must-revalidate
+strict-transport-security: max-age=86400; preload
+vary: User-Agent
+cf-cache-status: DYNAMIC
+expect-ct: max-age=604800, report-uri="https://report-uri.cloudflare.com/cdn-cgi/beacon/expect-ct"
+server: cloudflare
+cf-ray: 57f51d67cc0e824d-KBP
+'''
